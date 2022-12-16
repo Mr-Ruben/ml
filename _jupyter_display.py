d7832be142cde9089a4946d4ae79b86d91d19701
Mr Ruben <37179353+Mr-Ruben@users.noreply.github.com>
2022-12-16 17:59:58 +0100

    Adapted to the differences in library paths from Python 3.7

--- _jupyter_display.py
+++ _jupyter_display.py
@@ -2,11 +2,50 @@
 # !wget --no-clobber --quiet "https://raw.githubusercontent.com/Mr-Ruben/ml/master/_jupyter_display.py"
 # %run _jupyter_display.py
 
+import sys
+
+# Adapted to the differences in library paths from Python 3.7
+
+if sys.version_info.minor<=7:
+    from IPython.core.interactiveshell import InteractiveShell
+else:
+    from IPython import InteractiveShell
+
 # To print the output of all commands (except those ending in ; )
-from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
-# To change the width of the current notebook you're working on, you can enter the following into a cell:
-from IPython.core.display import display, HTML
+
+# To change the width of the current notebook you're working on,
+# you can enter the following into a cell:
+if sys.version_info.minor<=7:
+    from IPython.core.display import display, HTML
+else:
+    from IPython.display import display, HTML
+    
 display(HTML("<style>.container { width:100% !important; }</style>"))
+
+
+
+## To print the output of all commands (except those ending in ; )
+#from IPython.core.interactiveshell import InteractiveShell
+#InteractiveShell.ast_node_interactivity = "all"
+
+## To change the width of the current notebook you're working on, you can enter the following into a cell:
+#from IPython.core.display import display, HTML
+#display(HTML("<style>.container { width:100% !important; }</style>"))
+
+
+
+# ---------------------- v3 ---------------------------
+# import sys
+
+# if sys.version_info.minor<=7:
+#     from IPython.core.interactiveshell import InteractiveShell
+# else:
+#     from IPython.interactiveshell import InteractiveShell
+# InteractiveShell.ast_node_interactivity = "all"
+
+# # To change the width of the current notebook you're working on, you can enter the following into a cell:
+# from IPython.core.display import display, HTML
+# display(HTML("<style>.container { width:100% !important; }</style>"))
 
